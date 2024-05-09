1df2731ab630e836ac1a3c5db11c456124e5ac86
Ajay Kondisetty <ajay.kondisetty@thoughtworks.com>
2024-05-09 19:36:42 +0530

    [Manaswinee/Ajay] revert mistakenly added changes to book list API response format and enable CORS to books controller

--- src/main/java/com/tw/bootcamp/bookshop/book/BookController.java
+++ src/main/java/com/tw/bootcamp/bookshop/book/BookController.java
@@ -1,11 +1,15 @@
 package com.tw.bootcamp.bookshop.book;
 
+import com.tw.bootcamp.bookshop.apiUtil.APIResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
     private final BookService bookService;
 
@@ -29,10 +34,11 @@ public class BookController {
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = BookResponse.class))})
     })
-    List<BookResponse> list() {
+    ResponseEntity<APIResponse> list() {
         List<Book> books = bookService.fetchAll();
-        return books.stream()
+        APIResponse apiResponse = new APIResponse(HttpStatus.OK, books.stream()
                 .map(book -> book.toResponse())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
 }
